3a63388ae0352563b6b576513b2adc366e99014e
David Gascueña <davidgasku@gmail.com>
2025-04-03 09:01:37 +0200

    movement types

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -3,15 +3,19 @@ extends Control
 @onready var button_sound: AudioStreamPlayer = $ButtonSound
 
 
-func _on_play_pressed() -> void:
+func _on_8way_pressed() -> void:
 	button_sound.play()
-	get_tree().change_scene_to_file("res://scenes/ball1.tscn")
+	get_tree().change_scene_to_file("res://scenes/movements/8way_movement.tscn")
 
 
-func _on_settings_pressed() -> void:
+func _on_mouse_pressed() -> void:
 	button_sound.play()
-	get_tree().change_scene_to_file("res://scenes/ball2.tscn")
+	get_tree().change_scene_to_file("res://scenes/movements/mouse_movement.tscn")
 
+
+func _on_car_pressed() -> void:
+	button_sound.play()
+	get_tree().change_scene_to_file("res://scenes/movements/car_movement.tscn")
 
 
 func _on_quit_pressed() -> void:
